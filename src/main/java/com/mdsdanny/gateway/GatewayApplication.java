954f7edc46423a6dad86613dce1cff2900a185b3
Daniela Sanchez <mds.danny@gmail.com>
2021-03-04 12:32:33 -0300

    Initial commit

---
+++ src/main/java/com/mdsdanny/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.mdsdanny.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableHystrix
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mdsdanny/gateway/RoutesTest.java
@@ -0,0 +1,4 @@
+package com.mdsdanny.gateway;
+
+public class RoutesTest {
+}
